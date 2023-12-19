b779abe5aabf08bbea3abd01fe93eb9c76bce593
Archishmaan <archishmaan.upa4@gmail.com>
2023-12-19 17:27:41 +0530

    imported required modules and connected httpClient to display endpoint

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { AddtasksComponent } from './addtasks/addtasks.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { EdittasksComponent } from './edittasks/edittasks.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { EdittasksComponent } from './edittasks/edittasks.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class TodoService {
 
-  constructor() { }
+  constructor( private httpClient: HttpClient){
+
+  }
+
+  getTasks(){
+    return this.httpClient.get("http://localhost:3000/display")
+  }
 }
