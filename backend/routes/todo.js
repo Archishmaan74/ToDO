c2fe46d6e324a5776e7289a6093aa1264daced60
Archishmaan <archishmaan.upa4@gmail.com>
2023-12-19 19:33:41 +0530

    created edit and add APIs and edited tasks component

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -27,4 +27,37 @@ router.get('/display',(req,res)=>{
     })
 })
 
+router.post('/add',(req,res)=>{
+    console.log("Add task API getting hit...");
+    dbConnection.collection('tasks').find({sno:req.body.sno}).toArray((err,data)=>{
+        if(err){
+            console.log("Cannot add task...");
+        }
+        else{
+            dbConnection.collection('tasks').insertOne({
+                ...req.body
+            })
+            res.send("Task inserted...")
+        }
+    })
+})
+
+router.post('/edit',(req,res)=>{
+    console.log("Edit API getting hit...");
+    dbConnection.collection('tasks').find({sno:req.body.sno}).toArray((err,data)=>{
+        if(err){
+            console.log("Cannot fetch tasks to edit...");
+        }
+        else{
+            if(data.length > 0){
+                dbConnection.collection('tasks').updateOne({sno:req.body.sno},{$set:{...req.body}})
+                res.send("Editted task...")
+            }
+            else{
+                console.log("Cannot edit...");
+            }
+        }
+    })
+})
+
 module.exports = router
