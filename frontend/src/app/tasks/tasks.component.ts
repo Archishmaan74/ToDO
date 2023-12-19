ed987c5495e5cab146fb3aaad4523c692dfe521b
Archishmaan <archishmaan.upa4@gmail.com>
2023-12-19 18:17:55 +0530

    Subscribed data from service & created tasks component

--- frontend/src/app/tasks/tasks.component.ts
+++ frontend/src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-tasks',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
-
+  tasks: any=[]
+  constructor( private todoService : TodoService ){
+    this.todoService.getTasks().subscribe((data)=>{
+      this.tasks = data
+    },(err)=>{
+      console.log("Cannot fetch data...");
+    })
+  }
 }
