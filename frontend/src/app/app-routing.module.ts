de70b70d2e32585a756119aedeb3f839319ec254
Archishmaan <archishmaan.upa4@gmail.com>
2023-12-19 17:45:03 +0530

    created routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+import { AddtasksComponent } from './addtasks/addtasks.component';
+import { EdittasksComponent } from './edittasks/edittasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: TasksComponent
+  },
+  {
+    path: "addtasks", component: AddtasksComponent
+  },
+  {
+    path: "edittasks", component: EdittasksComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
